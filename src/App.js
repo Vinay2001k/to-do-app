bb87fe4adea20f14448a239eae3d9359291e9713
VihanCh <vinay70559900701@gmail.com>
2023-05-10 19:26:27 +0530

    Added Buttons

--- src/App.js
+++ src/App.js
@@ -7,8 +7,20 @@ function App() {
      <div className='todo-wrapper'>
       <div className='todo-input'>
         <div className='input-items'>
+          <label>Enter </label>
           <input type='text' placeholder="what's the task?"/>
         </div>
+        <div className='input-items'>
+          <label>Description</label>
+          <input type='text' placeholder="what's the task Description?"/>
+        </div>
+        <div className='input-items'>
+          <button type='button' className='PrimaryBtn'>Add</button>
+        </div>
+      </div>
+      <div className='btn-area'>
+        <button>Todo</button>
+        <button>Completed</button>
       </div>
      </div>
     </div>
