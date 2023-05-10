fff0279c9785119c7d54cb067ccf532a87a27b64
VihanCh <vinay70559900701@gmail.com>
2023-05-10 19:19:05 +0530

    input bar

--- src/App.js
+++ src/App.js
@@ -4,6 +4,13 @@ function App() {
   return (
     <div className="App">
      <h1>my todo</h1>
+     <div className='todo-wrapper'>
+      <div className='todo-input'>
+        <div className='input-items'>
+          <input type='text' placeholder="what's the task?"/>
+        </div>
+      </div>
+     </div>
     </div>
   );
 }
